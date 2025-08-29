47320569a1cde8a47ac201ebf53077bcaa0daed9
burnttoasttwitch-jpg <burnttoasttwitch@gmail.com>
2025-08-29 03:18:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from database import SessionLocal, engine, Base
+from models import User
 
+Base.metadata.create_all(bind=engine)
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Render with Waitress!"
+@app.route("/users")
+def users():
+    session = SessionLocal()
+    all_users = session.query(User).all()
+    return render_template("users.html", users=all_users)
 
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
-
-
+@app.route("/user/<int:user_id>")
+def user_detail(user_id):
+    session = SessionLocal()
+    user = session.query(User).filter_by(id=user_id).first()
+    return render_template("user.html", user=user)
